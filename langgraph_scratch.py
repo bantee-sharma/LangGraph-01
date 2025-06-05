84469e70af6c4822d9d6380080416c2193271477
bantee-sharma <banteesharma9@gmail.com>
2025-06-05 21:49:05 +0530

    Update langgraph_scratch.py

--- langgraph_scratch.py
+++ langgraph_scratch.py
@@ -1,10 +1,11 @@
 from langgraph.graph import Graph
+from IPython.display import Image, display
 
 def func1(input1):
-    return input1 + "This is from 1st func"
+    return input1 + " This is from 1st func"
 
 def func2(input2):
-    return input2 + "This is from func2"
+    return input2 + " sharma This is from func2"
 
 workflow1 = Graph()
 
@@ -17,3 +18,21 @@ workflow1.set_entry_point("func1")
 workflow1.set_finish_point("func2")
 
 app1 = workflow1.compile()
+
+'''try:
+    print(app1.get_graph().draw_ascii())
+except Exception as e:
+    # This requires some extra dependencies and is optional
+    print(e)'''
+
+#print(app1.invoke("Hii this is bantee"))
+
+input = "hii this is bantee"
+
+for output in app1.stream(input):
+    for key,value in output.items():
+        print("This output coming from {key}")
+        print("________")
+        print(value)
+        print("_________")
+    
