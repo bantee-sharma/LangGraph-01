073b1509f3a2463e43e3bfe79daa7479d159507e
bantee-sharma <banteesharma9@gmail.com>
2025-06-06 21:43:14 +0530

    Update with_rag.py

--- with_rag.py
+++ with_rag.py
@@ -18,6 +18,7 @@ chunks = text_splitter.split_documents(docs)
 
 embedd = HuggingFaceEmbeddings()
 db = FAISS.from_documents(chunks,embedd)
+
 retriver = db.as_retriever(search_type="similarity",kwargs={"k":3})
 query = "What is sql"
 retriver_docs = retriver.invoke(query)
