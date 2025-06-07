984660794ce04471a0cc2d601fa5281266139526
bantee-sharma <banteesharma9@gmail.com>
2025-06-07 21:25:02 +0530

    Update react_agent_with_lg.py

--- react_agent_with_lg.py
+++ react_agent_with_lg.py
@@ -1,7 +1,8 @@
 from langchain_google_genai import  ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 from langchain_community.tools import DuckDuckGoSearchRun
-
+from langchain_core.messages import HumanMessage,SystemMessage
+from langgraph.graph import MessagesState,StateGraph,END,START
 
 load_dotenv()
 llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
@@ -34,3 +35,5 @@ tools = [add, multiply, divide, search]
 llm_with_tools = llm.bind_tools(tools=tools)
 
 print(llm_with_tools.invoke("trending news in india"))
+
+system_msg = SystemMessage(content="You are a helpful assistant tasked with using search and performing arithmetic on a set of inputs.")
